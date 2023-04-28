c5e99aa2e892b0984948fb6fb9d983e89a7aab1e
marshmallow <marshycity@gmail.com>
2023-04-28 17:09:29 +1000

    nvim: add distant.nvim

--- private_dot_config/nvim/lua/marshmallow/plugins/editor.lua
+++ private_dot_config/nvim/lua/marshmallow/plugins/editor.lua
@@ -1,6 +1,15 @@
 return {
-	{
-		"folke/neodev.nvim",
-		opts = true
-	},
+    {
+        "folke/neodev.nvim",
+        opts = true
+    },
+    {
+        "chipsenkbeil/distant.nvim",
+        branch = "v0.2",
+        config = function()
+          require('distant').setup {
+              ['*'] = require('distant.settings').chip_default()
+          }
+        end
+    }
 }
