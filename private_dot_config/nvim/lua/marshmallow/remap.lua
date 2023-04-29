e2138ebecbdfeda22cdf01a5859d7cd3c4abb7fe
marshmallow <marshycity@gmail.com>
2023-04-29 17:42:43 +1000

    nvim: add splits naviagation bindings

--- private_dot_config/nvim/lua/marshmallow/remap.lua
+++ private_dot_config/nvim/lua/marshmallow/remap.lua
@@ -1,1 +1,6 @@
 vim.keymap.set('n', '<C-s>', ':w<cr>', { silent = true })
+
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", { noremap = true, silent = true });
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", { noremap = true, silent = true });
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", { noremap = true, silent = true });
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", { noremap = true, silent = true });
