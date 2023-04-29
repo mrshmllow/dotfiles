09bedab154ad2e14162cc52dbd94ec91ef51887e
marshmallow <marshycity@gmail.com>
2023-04-29 18:04:01 +1000

    nvim: add blend and keymaps

--- private_dot_config/nvim/lua/marshmallow/neovide.lua
+++ private_dot_config/nvim/lua/marshmallow/neovide.lua
@@ -6,5 +6,19 @@ if vim.g.neovide then
 
 	vim.g.neovide_hide_mouse_when_typing = true
 
-  vim.g.neovide_cursor_animation_length = 0.13
+	vim.g.neovide_cursor_animation_length = 0.13
+
+	vim.g.winblend = 30
+	vim.g.pumblend = 30
+
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	end
+
+	vim.keymap.set("n", "<C-=>", function()
+		change_scale_factor(1.25)
+	end)
+	vim.keymap.set("n", "<C-->", function()
+		change_scale_factor(1 / 1.25)
+	end)
 end
