a2e12480cc5f012f18353c6c3a443801c89590c1
marshmallow <marshycity@gmail.com>
2023-04-29 17:44:07 +1000

    nvim: fix some catppuccin loading config issues

--- private_dot_config/nvim/lua/marshmallow/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/marshmallow/plugins/colorscheme.lua
@@ -1,13 +1,19 @@
 return {
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			term_colors = true,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		term_colors = true,
+		intergration = {
+			neotree = true,
+      gitsigns = true,
+      treesitter_context = true,
+      treesitter = true,
 		},
-		config = function (opts)
-			require("catppuccin").setup(opts)
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
 
-			vim.cmd.colorscheme("catppuccin")
-		end
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
