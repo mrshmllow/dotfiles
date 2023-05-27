09784f607bf0ceef011fa7dd27760b9f2fe145a5
marshmallow <marshycity@gmail.com>
2023-05-27 19:31:25 +1000

    nvim: large update

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,1 +1,6 @@
+vim.g.use_lua_gx = true
+
+vim.g.lua_net_ft_full = true
+vim.g.lua_net_enable = true
+
 require("marshmallow")
--- private_dot_config/nvim/lua/marshmallow/defaults.lua
+++ private_dot_config/nvim/lua/marshmallow/defaults.lua
@@ -11,6 +11,8 @@ vim.o.smartcase = true
 
 vim.o.termguicolors = true
 
+vim.o.showmode = false
+
 vim.o.expandtab = true
 -- number of spaces used for each indentation level
 vim.o.shiftwidth = 2
@@ -19,12 +21,19 @@ vim.o.softtabstop = 2
 -- number of spaces a tab character appears as
 vim.o.tabstop = 2
 
+-- I use nf-fa nerd icons
 vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
 vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
 
 vim.o.cursorline = true
 
 vim.o.splitbelow = true
 vim.o.splitright = true
+
+vim.o.laststatus = 3
+
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,terminal,options"
+
+vim.cmd("cabbrev help tab help")
